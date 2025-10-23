985bf55771a61f4508f99efd3ba907d5031d746e
Kaden Benzley <ksbenzley2@gmail.com>
2025-10-23 14:24:38 -0600

    adding react to login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { useNavigate, BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { AuthState } from './authState'
 
 export function Login() {
 
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [authState, setAuthState] = useState(AuthState.Unauthenticated);
+
+  const navigate = useNavigate();
+
+  function handleLogin(e){
+    e.preventDefault();
+
+     if (userName.trim() && password.trim()) {
+      setAuthState(AuthState.Authenticated);
+      console.log('Logged in as', userName);
+      navigate('/explore');
+    } else {
+      alert('Please enter a username and password.');
+    }
+  }
+
   return (
     <div className="loginBody">
-    <main className="page-container">
-        <form className="login-form">
+      <main className="page-container">
+        <form className="login-form" onSubmit={handleLogin}>
               <div className="form-group">
                   <label>Username:</label>
-                  <input type="text" /><br/><br/>
+                  <input
+                    type="text"
+                    value = {userName}
+                    onChange = {(e) => setUserName(e.target.value)}
+                    />
+                    <br />
+                    <br />
           
                   <label>Password:</label>
-                  <input type="password" /><br/><br/>
+                  <input 
+                    type="password" 
+                    value = {password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <br />
+                    <br />
               </div>
 
               <div className="button-group">
-                  <NavLink to="/" className="nav-link">Log In</NavLink>
-                  <NavLink to="/createAccount" className="nav-link">Create Account</NavLink>
-                  <NavLink to="/create" className="nav-link">Take Me Home</NavLink>
+                <button type="submit" className="nav-link">Log In</button>
+                <NavLink to="/createAccount" className="nav-link">
+                  Create Account
+                </NavLink>
+                <NavLink to="/create" className="nav-link">
+                  Take Me Home
+                </NavLink>
               </div>
           </form>
-    </main>
+      </main>
     </div>
   );
 }
